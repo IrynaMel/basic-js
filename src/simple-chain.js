cfa832580810f229dc3637496539cbd6fb36413d
IrynaMel <iryna.melnyk270692@gmail.com>
2022-10-13 14:47:09 +0300

    add chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,32 +1,54 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Implement chainMaker object according to task description
- * 
+ *
  */
+
+// const
 const chainMaker = {
+  chain: [],
   getLength() {
-    throw new NotImplementedError('Not implemented');
+    return this.chain.length;
+    // throw new NotImplementedError("Not implemented");
     // remove line with error and write your code here
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(values) {
+    this.chain.push("( " + values + " )");
+    return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+    try {
+      if (
+        position < 1 ||
+        position > this.getLength() ||
+        isNaN(position) ||
+        !position
+      ) {
+        throw new ReferenceError("CORRECT");
+      } else {
+        const first = this.chain.slice(0, position - 1);
+        const second = this.chain.slice(position);
+        this.chain = [...first, ...second];
+        return this;
+      }
+    } catch (error) {
+      this.chain = [];
+      throw new NotImplementedError(error.message);
+    }
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.chain = this.chain.reverse();
+    return this;
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    const result = [...this.chain];
+    this.chain = [];
+
+    return result.join("~~");
+  },
 };
 
 module.exports = {
-  chainMaker
+  chainMaker,
 };
